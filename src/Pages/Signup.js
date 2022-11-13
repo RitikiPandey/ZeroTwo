364e008b3708481b64b140a66aebcedb52ab430b
Rajan kumar <krcpr007@gmail.com>
2022-11-13 16:23:21 +0530

    modify

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -27,19 +27,19 @@ function Signup() {
                                     <div className="p-2 w-full">
                                         <div className="relative">
                                             <label htmlFor="date" className="leading-7 text-sm text-gray-600">Email</label>
-                                            <input type="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder='email' />
+                                            <input type="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder='Email' />
                                         </div>
                                     </div>
                                     <div className="p-2 w-full">
                                         <div className="relative">
                                             <label htmlFor="date" className="leading-7 text-sm text-gray-600">Password</label>
-                                            <input type="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder='password' />
+                                            <input type="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder='Password' />
                                         </div>
                                     </div>
                                     <div className="p-2 w-full">
                                         <div className="relative">
                                             <label htmlFor="file" className="leading-7 text-sm text-gray-600">Confirm password</label>
-                                            <input type="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder='confirm password' />
+                                            <input type="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder='Confirm password' />
                                         </div>
                                     </div>
                                     <p className='text-sm px-2'>Already have account? <Link to="/login" className='underline'>login</Link></p>
