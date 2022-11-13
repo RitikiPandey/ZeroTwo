55a71947a94c7522e3743d7e84bb5691818731f8
Rajan kumar <krcpr007@gmail.com>
2022-11-13 16:21:02 +0530

    Signup page created

---
+++ src/Pages/Signup.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+function Signup() {
+    return (
+        <>
+            <div className="min-h-screen py-6 flex bg-gray-900 flex-col justify-center sm:py-12 pattern">
+                <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+                    <div
+                        className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+                    </div>
+                    <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+                        <div className="max-w-md mx-auto">
+                            <form>
+                                <div className="flex flex-wrap -m-2">
+                                    <div className="p-2 w-1/2">
+                                        <div className="relative">
+                                            <label htmlFor="name" className="leading-7 text-sm text-gray-600">First Name</label>
+                                            <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="First name" />
+                                        </div>
+                                    </div>
+                                    <div className="p-2 w-1/2">
+                                        <div className="relative">
+                                            <label htmlFor="email" className="leading-7 text-sm text-gray-600">Last Name</label>
+                                            <input type="text" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="Last name" />
+                                        </div>
+                                    </div>
+                                    <div className="p-2 w-full">
+                                        <div className="relative">
+                                            <label htmlFor="date" className="leading-7 text-sm text-gray-600">Email</label>
+                                            <input type="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder='email' />
+                                        </div>
+                                    </div>
+                                    <div className="p-2 w-full">
+                                        <div className="relative">
+                                            <label htmlFor="date" className="leading-7 text-sm text-gray-600">Password</label>
+                                            <input type="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder='password' />
+                                        </div>
+                                    </div>
+                                    <div className="p-2 w-full">
+                                        <div className="relative">
+                                            <label htmlFor="file" className="leading-7 text-sm text-gray-600">Confirm password</label>
+                                            <input type="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder='confirm password' />
+                                        </div>
+                                    </div>
+                                    <p className='text-sm px-2'>Already have account? <Link to="/login" className='underline'>login</Link></p>
+                                    <div className="p-2 w-full">
+                                        <button className="flex mx-auto border hover:shadow-sky-500 border-sky-500 px-3 py-1 font-medium rounded-lg  text-sky-500 hover:bg-sky-500 hover:text-slate-900  text-lg" type='submit' >Signup</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Signup
