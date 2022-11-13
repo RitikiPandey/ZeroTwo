1572267af5f87b21036c2c63ad41972a68dcc509
Rajan kumar <krcpr007@gmail.com>
2022-11-13 08:37:48 +0530

    Login created routes added

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Login() {
     return (
         <>
-            <div className="min-h-screen py-6 flex bg-white dark:bg-[#181F2A] flex-col justify-center sm:py-12 pattern">
+            <div className="min-h-screen py-6 flex bg-gray-900 flex-col justify-center sm:py-12 pattern">
                 <div className="relative py-3 sm:max-w-xl sm:mx-auto">
                     <div
                         className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
@@ -17,7 +17,7 @@ function Login() {
                                 <div className="divide-y divide-gray-200">
                                     <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                                         <div className="relative">
-                                            <input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
+                                            <input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email" />
                                             <label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
                                         </div>
                                         <div className="relative">
