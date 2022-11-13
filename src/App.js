18e2f27ec1d17db2d32cf6fdbdf0bff7b7456307
sneha <sneha113kumari@gmail.com>
2022-11-13 15:26:29 +0530

    landing page responsive

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import LandingPage from './Components/LandingPage';
 import Footer from './Components/Footer';
 import Login from "./Pages/Login";
-import Dashboard from "./Pages/Dashboard";
+
 
 
 function App() {
@@ -15,7 +15,6 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
       <Footer />
     </>
